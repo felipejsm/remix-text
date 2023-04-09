e9efdd9232899180cade278a828751eab915f9a3
felipejsm <felipejsm41@gmail.com>
2023-04-09 15:35:11 -0300

    adding post route

--- onewhee-blog/app/routes/_index.tsx
+++ onewhee-blog/app/routes/_index.tsx
@@ -66,6 +66,12 @@ export default function Index() {
           </div>
         </div>
 
+        <div className="mx-auto mt-16 max-w-7x1 text-center">
+          <Link to="/posts" className="text-x1 text-blue-600 underline">
+            blog post
+          </Link>
+        </div>
+
         <div className="mx-auto max-w-7xl px-4 py-2 sm:px-6 lg:px-8">
           <div className="mt-6 flex flex-wrap justify-center gap-8">
             {[
